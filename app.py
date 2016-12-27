e4ba2ce321611afdd4865fe14e4dd97fd2257685
Amit Gupta <hriks@outlook.com>
2016-12-27 22:17:10 +0530

    initail commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template,\
     request, url_for, flash, session
-import db
+from db import *
 
 app = Flask(__name__)
 app.secret_key = 'F12Zr47j\3yX R~X@H!jfmM]Lwf/,?KT'
@@ -11,23 +11,21 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/log?usr?get', methods=['GET', 'POST'])
-def login():
+@app.route('/logina', methods=['GET', 'POST'])
+def logina():
     error = None
     if request.method == 'POST':
-        value = db.authenticate(
+        value = authenticate_user(
             request.form['username'],
             request.form['password'])
         if (value == 1):
             print "login Succesfully"
-            return render_template(
-                'logged.html',
-                error=error)
+            return render_template('logged.html')
         else:
             error = 'Invalid username or password \
             Please try again!'
-            return render_template('logged.html', session=session)
-    return render_template('logged.html')
+            return render_template('index.html', error=error)
+    return render_template('index.html', error=error)
 
 
 @app.route('/reg?me?chat2')
@@ -42,16 +40,20 @@ def contact():
 
 @app.route('/registerme', methods=['POST'])
 def registerme():
-    a = request.form['userid']
-    dataaa = db.register_me(a, request.form['referralid'], request.form['name'], request.form['email'], request.form['phone'], request.form['password'])
-    if (dataaa == 1):
-        return redirect(url_for('login'))
+    data = register_me(request.form['userid'],
+                       request.form['referralid'],
+                       request.form['fullname'],
+                       request.form['emailid'],
+                       request.form['phone'],
+                       request.form['password'])
+    if (data == 1):
+        return render_template('register.html')
     else:
-        return redirect(url_for('login'))
-    return redirect(url_for('registration'))
+        return render_template('register.html')
+    return render_template('register.html')
 
 
-@app.route('/login')
+@app.route('/loggeds')
 def index():
     error = None
     try:
@@ -76,50 +78,19 @@ def index():
         raise e
 
 
-@app.route('/add')
-def add():
-    return render_template('add_catagories.html')
-
-
 @app.route('/password??')
 def password_help():
     return render_template('forgot_pass.html')
 
 
+@app.route('/logged')
+def user_page():
+    return render_template('logged.html')
+
+
 @app.route('/faq??')
 def faq():
     return render_template('faq.html')
-
-
-@app.route("/catagory", methods=['POST'])
-def catagory():
-    error = None
-    data_catagory = db.catagory_alreadyexits(session['name'],
-                                             request.form['field7'],
-                                             request.form['field8'],
-                                             request.form['field9'])
-    if (data_catagory == 1):
-        flash('ERROR! CATAGORY ALREADY SATISFIED OR SOMETHING WENT WRONG PLEASE CHECK NEXT MESSAGE TO CONFIRM')
-    else:
-        flash(data_catagory)
-        user_data = session['name']
-        print user_data
-        data = db.filter_user_data(user_data)
-        data_user_get = db.filter_user_chart(user_data)
-        graph_data = []
-        for elem in data_user_get:
-            cat = elem[0]
-            exp = elem[1]
-            li = [cat, int(exp)]
-            graph_data.append(li)
-        graph_data.insert(0, ['Category', 'Expenses'])
-        return render_template(
-            'index.html',
-            error=error,
-            data=data,
-            data_chart=graph_data)
-    flash('REQUEST PERFORMED!')
-    return redirect('add')
 
 
 @app.route('/asd')
--- db.py
+++ db.py
@@ -11,27 +11,6 @@ def get_connection():
     return conn
 
 
-def create_db():
-    try:
-        connection = get_connection()
-        cursor = connection.cursor()
-        cursor.execute('''CREATE TABLE LOGS
-        (USERID       TEXT    UNIQUE    NOT Null,
-         NAME           TEXT    NOT NULL,
-         EMAIL            TEXT     NOT NULL,
-         PASSWORD        TEXT);''')
-        print "Table created successfully"
-        cursor.execute('''CREATE TABLE LOGSS
-         (USERID       TEXT   REFERENCES LOGS(USERID)  NOT Null,
-            CATAGORIES           TEXT   UNIQUE   NOT NULL,
-             PRICE            INT     NOT NULL,
-            DESCRIPTION        TEXT);''')
-        connection.commit()
-        connection.close()
-    except Exception as error:
-        return error
-
-
 def register_me_input(USERID, REFRALID, NAME, EMAIL, PHONENO, PASSWORD):
     connection = get_connection()
     cursor = connection.cursor()
@@ -66,52 +45,7 @@ def register_me(USERID, REFRALID, NAME, EMAIL, PHONENO, PASSWORD):
         return error
 
 
-def filter_user_data(USER):
-    connection = get_connection()
-    cursor = connection.cursor()
-    fetch_db = """SELECT USERID, CATAGORIES, PRICE,
-     DESCRIPTION  from LOGSS where USERID='%s'"""
-    fetch_db = fetch_db % (USER)
-    cursor.execute(fetch_db)
-    rows = cursor.fetchall()
-    print "Operation done successfully"
-    cursor.close()
-    return rows
-
-
-def insert_db(USER, NAME, EMAIL, PASSWORDV):
-    connection = get_connection()
-    cursor = connection.cursor()
-    print "cur is created"
-    query = """INSERT INTO LOGS(USERID,NAME,
-    EMAIL,PASSWORD) VALUES('%s', '%s', '%s', '%s');"""
-    query = query % (
-        USER, NAME, EMAIL, PASSWORDV)
-    print query
-    cursor.execute(query)
-    connection.commit()
-    print "Records created successfully"
-    connection.close()
-
-
-def user_alreadyexits(USER, NAME, EMAIL, PASSWORDV):
-    connection = get_connection()
-    cursor = connection.cursor()
-    query = """SELECT USERID from LOGS where USERID='%s';"""
-    query = query % (USER, )
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    try:
-        if (len(rows) == 0):
-            insert_db(USER, NAME, EMAIL, PASSWORDV)
-        else:
-            return 1
-    except Exception as error:
-        return error
-    connection.close()
-
-
-def authenticate(username, password):
+def authenticate_user(username, password):
     connection = get_connection()
     cursor = connection.cursor()
     query = """SELECT USERID, PASSWORD from LOGS
@@ -119,11 +53,11 @@ def authenticate(username, password):
     query = query % (username, password)
     cursor.execute(query)
     rows = cursor.fetchall()
+    connection.close()
     try:
         if (rows[0][0] == username) and (rows[0][1] == password):
             return 1
         else:
             return 0
-    except Exception as error:
-        return error
-    connection.close()
+    except Exception as e:
+        return e
